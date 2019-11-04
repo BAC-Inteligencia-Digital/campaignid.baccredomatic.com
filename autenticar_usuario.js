18e357ef164ebfa7d55c18ab27a483bf15cd94b0
nanonav77 <navrojd77@gmail.com>
2019-11-04 16:06:19 -0600

    código para realizar la autenticación de usuario

---
+++ autenticar_usuario.js
@@ -0,0 +1,44 @@
+
+document.querySelector('#autenticacionBoton').addEventListener('click', autenticarUsuario);
+
+//var conexion_capa_datos = "https://bac-id-new.azurewebsites.net/";
+
+var capa_datos4 = "consulta_login/";
+
+function autenticarUsuario(){
+
+    var id;
+
+    var usuario = 'jmurillo' ; //aquí se captura el usuario ingresado 
+    var contrasena = 'jorge45' ; //aquí se captura la contraseña ingresada
+    
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET',capa_datos4+'autenticar_usuario.php?usuario_ingresado='+usuario+'&contrasena_ingresada='+contrasena,true);
+
+    xhttp.send();
+
+    
+    xhttp.onreadystatechange = function(){
+
+            if(this.readyState == 4 && this.status == 200){
+ 
+                    let datos = JSON.parse(this.responseText);
+           
+                    for(let item of datos){
+                         id = item.id; // si este ID es mayor a cero es por que el usuario está registrado
+                         if(id>0){
+                            console.log('registrado');
+                         }
+                         else{
+                            console.log('no registrado'); 
+                         }
+                         
+                    }
+                  
+                    
+            }
+            
+    }
+
+}
