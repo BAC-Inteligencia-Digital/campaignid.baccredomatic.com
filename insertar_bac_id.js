5e9bae8dec63d7ec0a3ea46efff182188df36c00
nanonav77 <navrojd77@gmail.com>
2019-11-05 16:07:34 -0600

    funciones para obtener los datos del bac id luego de ser insertado

--- insertar_bac_id.js
+++ insertar_bac_id.js
@@ -1,7 +1,7 @@
 
-document.querySelector('#insertar_bac_id').addEventListener('click', insertarBACID);
+document.querySelector('#insertar_bac_id').addEventListener('click', consultarBACID);
 
-var capa_datos3 = "https://bac-id-new.azurewebsites.net/insertar_bac_id/";
+var capa_datos3 = "insertar_bac_id/";
 
 function insertarBACID(){
 
@@ -22,4 +22,70 @@ function insertarBACID(){
             }
             
     }
+}
+
+function consultarBACID(){
+
+    var bac_id_registrado = 'GUAB-COMP-AML-123456-AA-aw-pr-wa-D4-zp'; //aqui te traes el valor del campo de bac_id
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET',capa_datos3+'consulta_bac_id_postinsercion.php?pais_bacid='+bac_id_registrado.substring(0,3)+
+    '&origen_bacid='+bac_id_registrado.substring(3,4)+'&categoria_bacid='+bac_id_registrado.substring(5,9)+
+    '&producto_bacid='+bac_id_registrado.substring(10,13)+'&portafolio_bacid='+bac_id_registrado.substring(21,23)+'&tipo_campana_bacid='+bac_id_registrado.substring(24,26)+
+    '&objetivo_bacid='+bac_id_registrado.substring(27,29),true);
+
+    xhttp.send();
+    
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+     
+            let datos = JSON.parse(this.responseText);
+
+            console.log("Pais: "+datos[0].nombre_pais);
+            console.log("Origen Clientes: "+datos[1].nombre_origen);
+            console.log("Categoria: "+datos[2].nombre_categoria);
+            console.log("Producto: "+datos[3].nombre_producto);
+            console.log("Portafolio: "+datos[4].nombre_portafolio);
+            console.log("Tipo Campaña: "+datos[5].nombre_campana);
+            console.log("Objetivo: "+datos[6].nombre_objetivo);
+            obtenerCanalesInsertados(bac_id_registrado.substring(14,20));
+        }
+            
+    }
+}
+
+function obtenerCanalesInsertados(codigo_canales){ //el codigo canales serían los ceros: 000000
+    
+    console.log(codigo_canales.substring(1,1));
+
+    var resultado;
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET',capa_datos3+'consulta_canales_insertados.php?canal1_seleccionado='+codigo_canales.substring(0,1)+
+    '&canal2_seleccionado='+codigo_canales.substring(1,2)+'&canal3_seleccionado='+codigo_canales.substring(2,3)+
+    '&canal4_seleccionado='+codigo_canales.substring(3,4)+'&canal5_seleccionado='+codigo_canales.substring(4,5)+
+    '&canal6_seleccionado='+codigo_canales.substring(5,6),true);
+
+    xhttp.send();
+    
+    xhttp.onreadystatechange = function(){
+
+            if(this.readyState == 4 && this.status == 200){
+
+                  let datos = JSON.parse(this.responseText);
+
+                  resultado = datos[0].nombre_canal1;  
+                  resultado = resultado +", "+datos[1].nombre_canal2;
+                  resultado = resultado +", "+ datos[2].nombre_canal3;
+                  resultado = resultado +", "+ datos[3].nombre_canal4; 
+                  resultado = resultado +", "+ datos[4].nombre_canal5; 
+                  resultado = resultado +" y "+ datos[5].nombre_canal6;    
+            }
+            console.log("Canales: "+resultado); //resultado devuele los canales seleccionados por los clientes
+    }
+    
+
 }
