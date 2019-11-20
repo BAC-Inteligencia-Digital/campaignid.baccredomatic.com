d4bcd4c44a6ad484e0d96d1515474b050281b24f
nanonav77 <navrojd77@gmail.com>
2019-11-20 16:49:55 -0600

    orden fecha

--- consulta_bacid_historicos/consulta_historico_bacids.php
+++ consulta_bacid_historicos/consulta_historico_bacids.php
@@ -21,32 +21,35 @@
     }*/
     
     $return_arr = array();
-	// creaci√≥n de la conexi√≥n a la base de datos con mysql_connect()
+	// creaci®Æn de la conexi®Æn a la base de datos con mysql_connect()
 	$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
 	
-	// Selecci√≥n del a base de datos a utilizar
+	// Selecci®Æn del a base de datos a utilizar
 	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
 	// establecer y realizar consulta. guardamos en variable.
-	$consulta = "select a.id,a.nombre_bac_id, a.nombre_campana, b.nombre as nombre_pais, c.nombre_origen, d.nombre_categoria, e.nombre_producto from bac_id_generados as a
-    join pais as b
-    on b.abreviatura = substring(a.nombre_bac_id,1,3)
-    join origen_clientes as c
-    on c.codigo = substring(a.nombre_bac_id,4,1)
-    join categoria as d
-    on d.codigo = substring(a.nombre_bac_id,6,4)
-    join producto as e
-    on e.codigo = substring(a.nombre_bac_id,11,3)
-    union all
-    select a.id,a.nombre_bac_id, a.nombre_campana, b.nombre as nombre_pais, c.nombre_origen, d.nombre_categoria, GROUP_CONCAT('[',e.nombre_campana,']') as nombre_producto from bac_id_generados as a
-        join pais as b
-        on b.abreviatura = substring(a.nombre_bac_id,1,3)
-        join origen_clientes as c
-        on c.codigo = substring(a.nombre_bac_id,4,1)
-        join categoria as d
-        on d.codigo = substring(a.nombre_bac_id,6,4) and 'MULT' = substring(a.nombre_bac_id,6,4)
-        join multiproducto as e
-        on e.codigo = substring(a.nombre_bac_id,11,1) or e.codigo = substring(a.nombre_bac_id,12,1) or e.codigo = substring(a.nombre_bac_id,13,1)
-    group by a.id";
+	$consulta = "select * from (
+        select a.id,a.nombre_bac_id, a.fecha_creacion,a.nombre_campana, b.nombre as nombre_pais, c.nombre_origen, d.nombre_categoria, e.nombre_producto from bac_id_generados as a
+            join pais as b
+            on b.abreviatura = substring(a.nombre_bac_id,1,3)
+            join origen_clientes as c
+            on c.codigo = substring(a.nombre_bac_id,4,1)
+            join categoria as d
+            on d.codigo = substring(a.nombre_bac_id,6,4)
+            join producto as e
+            on e.codigo = substring(a.nombre_bac_id,11,3)
+            union all
+            select a.id,a.nombre_bac_id, a.fecha_creacion,a.nombre_campana, b.nombre as nombre_pais, c.nombre_origen, d.nombre_categoria, GROUP_CONCAT('[',e.nombre_campana,']') as nombre_producto from bac_id_generados as a
+                join pais as b
+                on b.abreviatura = substring(a.nombre_bac_id,1,3)
+                join origen_clientes as c
+                on c.codigo = substring(a.nombre_bac_id,4,1)
+                join categoria as d
+                on d.codigo = substring(a.nombre_bac_id,6,4) and 'MULT' = substring(a.nombre_bac_id,6,4)
+                join multiproducto as e
+                on e.codigo = substring(a.nombre_bac_id,11,1) or e.codigo = substring(a.nombre_bac_id,12,1) or e.codigo = substring(a.nombre_bac_id,13,1)
+            group by a.id
+        ) tt
+        order by tt.fecha_creacion";
 	$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
 
    if ($conexion)
