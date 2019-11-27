95a99c6494dce50542cada3df71cbeda6a7af812
nanonav77 <navrojd77@gmail.com>
2019-11-27 15:54:34 -0600

    archivo conexion en insertar2

--- insertar_bac_id/consulta_bac_id_postinsercion.php
+++ insertar_bac_id/consulta_bac_id_postinsercion.php
@@ -1,5 +1,5 @@
 <?php
-	   include 'conexion_base_datos.php';
+	   include '../archivo_conexion_db/conexion_base_datos.php';
 	   
        $pais_bacid = $_GET['pais_bacid'];
        $origen_bacid = $_GET['origen_bacid'];
