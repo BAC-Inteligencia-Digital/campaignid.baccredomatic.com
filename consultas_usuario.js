ce7a7eab27ac1440c6ba42bb2e18b16a676785c6
José Navarro <navrojd77@gmail.com>
2020-03-05 14:00:54 -0600

    agregar funcionalidad buscar usuario

---
+++ consultas_usuario.js
@@ -0,0 +1,62 @@
+
+document.querySelector('#insercionBoton').addEventListener('click', insertarUsuario);
+document.querySelector('#buscarBoton').addEventListener('click', buscarUsuario);
+
+var capa_datos_insertaruser = "https://bac-id-new.azurewebsites.net/consultas_usuario/"
+
+
+function insertarUsuario(){
+
+    var usuario_red = document.getElementById("id_usuariored").value;
+    var contrasena = document.getElementById("id_contrasena").value;
+    var correo = document.getElementById("id_correo").value;
+    var tipo_usuario = document.getElementById("id_tipousuario").value;
+    var pais = document.getElementById("id_pais").value;
+    var nombre = document.getElementById("id_nombre").value;
+    var apellidos = document.getElementById("id_apellidos").value;
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET',capa_datos_insertaruser+'insertar_usuario.php?usuario_red='+usuario_red+'&contrasena='+contrasena
+    +'&correo='+correo+'&tipo_usuario='+tipo_usuario+'&pais='+pais+'&nombre='+nombre+'&apellidos='+apellidos,true);
+
+    xhttp.send();
+    
+    xhttp.onreadystatechange = function(){
+
+            if(this.readyState == 4 && this.status == 200){
+
+                    
+            }
+            
+    }
+}
+
+function buscarUsuario(){
+
+    var usuario_ingresado = "jose.navarror"; //aquí se manda el campo que corresponde al usuario de red o al correo 
+
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET',capa_datos_insertaruser+'buscar_usuario.php?usuario_ingresado='+usuario_ingresado,true);
+
+    xhttp.send();
+    
+    xhttp.onreadystatechange = function(){
+
+            if(this.readyState == 4 && this.status == 200){
+
+                for(let item of datos){
+                     console.log(item.id);
+                     console.log(item.usuario_red);
+                     console.log(item.contrasena);
+                     console.log(item.correo);
+                     console.log(item.nombre_usuario);
+                     console.log(item.apellidos_usuario);
+                     console.log(item.pais);
+                     console.log(item.tipo_usuario);
+               }   
+            }
+            
+    }
+}
---
+++ consultas_usuario/actualizar_usuario.php
@@ -0,0 +1,28 @@
+<?php
+
+    include '../archivo_conexion_db/conexion_base_datos.php';
+
+    $id_usuario = $_GET['id_usuario'];
+    $usuario_red = $_GET['usuario_red'];
+    $contrasena = $_GET['contrasena'];
+    $correo = $_GET['correo'];
+    $tipo_usuario = $_GET['tipo_usuario'];
+    $pais = $_GET['pais'];
+    $nombre = $_GET['nombre'];
+    $apellidos = $_GET['apellidos']; 
+
+    $conn = new mysqli( $servidor, $usuario, $password , $basededatos);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "update usuarios set usuario_red = '$usuario_red' , contraseña = '$contrasena', correo = '$correo', tipo_usuario = '$tipo_usuario' , pais = '$pais', nombre = '$nombre', apellidos= '$apellidos'
+    where id = '$id_usuario'";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+
+?>
