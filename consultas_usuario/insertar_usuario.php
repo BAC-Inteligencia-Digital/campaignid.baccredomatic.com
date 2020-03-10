308f50d5ab592cc5220a30665b8d5cb551ff13b7
José Navarro <navrojd77@gmail.com>
2020-03-10 16:03:17 -0600

    roolback insertar usuario

--- consultas_usuario/insertar_usuario.php
+++ consultas_usuario/insertar_usuario.php
@@ -2,7 +2,7 @@
 
     include '../archivo_conexion_db/conexion_base_datos.php';
 
-    $return_arr = array();
+
 
     $usuario_red = $_GET['usuario_red'];
     $contrasena = $_GET['contrasena'];
@@ -13,25 +13,10 @@
     $apellidos = $_GET['apellidos'];
     $estado = $_GET['estado'];
     
-    
-    $validar; //esta variable se encarga de validar si un correo o usuario de red existe
-
+     
     $conn = new mysqli( $servidor, $usuario, $password , $basededatos);
 
-    $consulta_validar = "SELECT * FROM usuarios where usuario_red = '$usuario_ingresado' or correo = '$correo'";
-    $resultado_validar = mysqli_query( $conn, $consulta_validar) or die ( "Algo ha ido mal en la consulta a la base de datos");
-    
-    if ($conn)
-    {
-        while($row1 = mysqli_fetch_array($resultado_validar)){
-              $validar = $row1['id'];
-              $row_array['validar'] = "nano";
-              array_push($return_arr,$row_array);
-        }  
-    }
-
-    if ($validar<1){
-        // Check connection for insert user
+    // Check connection for insert user
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -45,12 +30,8 @@
         }
     }
 
-    else{
-        $row_array['error'] = "El usuario o correo electrónico ya se encuentran registrados!";
-        array_push($return_arr,$row_array);
-    }
+   
     $conn->close();
 
-    echo json_encode($return_arr);
 
 ?>
