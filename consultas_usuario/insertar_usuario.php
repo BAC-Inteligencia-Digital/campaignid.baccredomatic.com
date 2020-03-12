fcbff6d4052ecf236fd9b6f4c31ce11e615d1b2a
José Navarro <navrojd77@gmail.com>
2020-03-12 14:36:08 -0600

    agregar validación existencia usuario

--- consultas_usuario/insertar_usuario.php
+++ consultas_usuario/insertar_usuario.php
@@ -2,6 +2,8 @@
 
     include '../archivo_conexion_db/conexion_base_datos.php';
 
+    $return_arr = array();
+
     $usuario_red = $_GET['usuario_red'];
     $contrasena = $_GET['contrasena'];
     $correo = $_GET['correo'];
@@ -16,13 +18,23 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "INSERT INTO usuarios (usuario_red,contraseña,correo,tipo_usuario,pais,nombre,apellidos,estado)
-    VALUES ('$usuario_red', '$contrasena','$correo','$tipo_usuario','$pais','$nombre','$apellidos','$estado')";
+    $sql = "INSERT INTO usuarios (usuario_red, contraseña, correo,tipo_usuario,pais,nombre,apellidos,estado)
+    SELECT * FROM (SELECT '$usuario_red', '$contrasena', '$correo','$tipo_usuario','$pais','$nombre','$apellidos','$estado') AS tmp
+    WHERE NOT EXISTS (
+        SELECT usuario_red,correo FROM usuarios WHERE usuario_red = '$usuario_red' or correo = '$correo'
+    ) LIMIT 1;";
+
     if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+        //echo "New record created successfully";
+        $row_array['mensaje'] = "New record created successfully" ;
+        array_push($return_arr,$row_array);
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        //echo "Error: " . $sql . "<br>" . $conn->error;
+        $row_array['mensaje'] = "Error: " . $sql . "<br>" . $conn->error;;
+        array_push($return_arr,$row_array);
     }
     $conn->close();
+
+    echo json_encode($return_arr);
 
 ?>
