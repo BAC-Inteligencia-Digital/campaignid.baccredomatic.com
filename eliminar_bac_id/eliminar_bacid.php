1584c45c4435bcdc62fa3bb4948d6b22d08065dd
jnavarrobac <jnavarroro@baccredomatic.com>
2021-03-22 14:40:12 -0600

    agregando bitacora eliminación BD

--- eliminar_bac_id/eliminar_bacid.php
+++ eliminar_bac_id/eliminar_bacid.php
@@ -2,17 +2,29 @@
    	include '../archivo_conexion_db/conexion_base_datos.php';
 
     $cod_bacid = $_GET['cod_bacid'];
+    $id_usuario = $_GET['id_usuario'];
 	
 	// creación de la conexión a la base de datos con mysql_connect()
 	$conn = new mysqli( $servidor, $usuario, $password , $basededatos); //or die ("No se ha podido conectar al servidor de Base de datos");
 	
 	// Selección del a base de datos a utilizar
-	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+	$db = mysqli_select_db( $conn, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
 	
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-      }
-      
+    }
+    
+    // INSERTAR BITACORA DE ELIMINACION
+
+    $sql0 = "INSERT INTO bitacora_eliminacion (id_bacid,nombre_bacid,nombre_campana,fecha,tipo,responsable)
+    VALUES ('$cod_bacid', SELECT nombre_bac_id from bac_id_generados where id = '$cod_bacid',SELECT nombre_campana from bac_id_generados where id = '$cod_bacid',CURDATE(),'bacid','$id_usuario')";
+
+    if ($conn->query($sql0) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
     // ELIMINAR PRIMERO LOS SUBBACIDS ASOCIADOS AL BAC ID
     $sql = "DELETE FROM sub_bacid_generados WHERE id_bacid_padre='$cod_bacid'";
       
@@ -31,6 +43,6 @@
         echo "Error deleting record: " . $conn->error;
     }
 
-    mysqli_close($conexion);
+    mysqli_close($conn);
 
 ?>
