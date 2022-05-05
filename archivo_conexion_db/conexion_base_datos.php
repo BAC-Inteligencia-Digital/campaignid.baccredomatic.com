093f6ad9d94e6d6eba2607b2ddd4bc461f1e2003
jnavarrobac <75701452+jnavarrobac@users.noreply.github.com>
2022-05-04 18:05:53 -0600

    renovate credentials

--- archivo_conexion_db/conexion_base_datos.php
+++ archivo_conexion_db/conexion_base_datos.php
@@ -1,5 +1,5 @@
 <?php
-// Datos de la base de datos
+// Credenciales de la base de datos
 	$usuario = "int_dig_mysqlusr@int-dig-mysql";
 	$password = "Credomatic.2020";
 	$servidor = "int-dig-mysql.mysql.database.azure.com:3306";
