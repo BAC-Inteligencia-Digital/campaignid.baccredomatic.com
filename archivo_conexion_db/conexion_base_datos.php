d480e056372e286191cfa4b6f9ff068f94be7784
José Navarro <jnavarroro@baccredomatic.com>
2023-08-22 20:55:08 -0600

    clean code backend

--- archivo_conexion_db/conexion_base_datos.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Credenciales de la base de datos
-	$usuario = "int_dig_mysqlusr@int-dig-mysql";
-	$password = "Credomatic.2020";
-	$servidor = "int-dig-mysql.mysql.database.azure.com:3306";
-	$basededatos = "base_datos_bac_id";
-?>
-
