85b988200d2b36c8f63129b7d443816f440afb69
José Navarro <jnavarroro@baccredomatic.com>
2023-08-24 10:01:55 -0600

    temp pass pro bck

---
+++ eliminar_bac_id/eliminar_bacid.php
@@ -0,0 +1,63 @@
+<?php
+   	include '../archivo_conexion_db/conexion_base_datos.php';
+
+    $cod_bacid = $_GET['cod_bacid'];
+    $id_usuario = $_GET['id_usuario'];
+
+    $nombre_codigo;
+    $nombre_campana;
+	
+	// creación de la conexión a la base de datos con mysql_connect()
+	$conn = new mysqli( $servidor, $usuario, $password , $basededatos); //or die ("No se ha podido conectar al servidor de Base de datos");
+	
+	// Selección del a base de datos a utilizar
+	$db = mysqli_select_db( $conn, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+	
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // EXTRAER PRIMERO LOS DATOS DEL BAC ID A ELIMINAR
+	$consulta = "SELECT nombre_bac_id,nombre_campana from bac_id_generados where id = '$cod_bacid'";
+	$resultado = mysqli_query( $conn, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conn)
+   {
+       while($row = mysqli_fetch_array($resultado)){
+          $nombre_codigo = $row['nombre_bac_id'];
+          $nombre_campana = $row['nombre_campana'];           
+       }  
+   }
+    
+    // INSERTAR BITACORA DE ELIMINACION
+
+    $sql0 = "INSERT INTO bitacora_eliminacion (id_bacid,nombre_bacid,nombre_campana,fecha,tipo,responsable)
+    VALUES ('$cod_bacid', '$nombre_codigo','$nombre_campana',CURDATE(),'bacid','$id_usuario')";
+
+    if ($conn->query($sql0) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // ELIMINAR PRIMERO LOS SUBBACIDS ASOCIADOS AL BAC ID
+    $sql = "DELETE FROM sub_bacid_generados WHERE id_bacid_padre='$cod_bacid'";
+      
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+
+    // ELIMINAR FINALMENTE EL BAC ID PADRE
+    $sql2 = "DELETE FROM bac_id_generados WHERE id='$cod_bacid'";
+      
+    if ($conn->query($sql2) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+
+    mysqli_close($conn);
+
+?>
---
+++ insertar_bac_id/consulta_canales_insertados.php
@@ -0,0 +1,131 @@
+<?php
+	   include '../archivo_conexion_db/conexion_base_datos.php';
+	   
+       $canal1_seleccionado = $_GET['canal1_seleccionado'];
+       $canal2_seleccionado = $_GET['canal2_seleccionado'];
+       $canal3_seleccionado = $_GET['canal3_seleccionado'];
+       $canal4_seleccionado = $_GET['canal4_seleccionado'];
+       $canal5_seleccionado = $_GET['canal5_seleccionado'];
+       $canal6_seleccionado = $_GET['canal6_seleccionado'];
+
+        $return_arr = array();
+	
+	// creación de la conexión a la base de datos con mysql_connect()
+	$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+	
+	// Selección del a base de datos a utilizar
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+	// establecer y realizar consulta. guardamos en variable.
+	$consulta1 = "SELECT nombre_canal FROM canales where codigo='$canal1_seleccionado'";
+	$resultado1 = mysqli_query( $conexion, $consulta1 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conexion and !$canal1_seleccionado=="0")
+   {
+       while($row = mysqli_fetch_array($resultado1)){
+
+        
+            $row_array['nombre_canal1'] = $row['nombre_canal'];
+            array_push($return_arr,$row_array);
+           
+       }  
+   }
+   else{
+      $row_array['nombre_canal1'] = "";
+      array_push($return_arr,$row_array);
+   }
+
+   $consulta2 = "SELECT nombre_canal FROM canales where codigo='$canal2_seleccionado'";
+	$resultado2 = mysqli_query( $conexion, $consulta2 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conexion and !$canal2_seleccionado=="0")
+   {
+       while($row = mysqli_fetch_array($resultado2)){
+
+            $row_array['nombre_canal2'] = $row['nombre_canal'];
+            array_push($return_arr,$row_array);
+           
+       }  
+   }
+   else{
+      $row_array['nombre_canal2'] = "";
+      array_push($return_arr,$row_array);
+   }
+
+   $consulta3 = "SELECT nombre_canal FROM canales where codigo='$canal3_seleccionado'";
+	$resultado3 = mysqli_query( $conexion, $consulta3 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conexion and !$canal3_seleccionado=="0")
+   {
+       while($row = mysqli_fetch_array($resultado3)){
+
+     
+            $row_array['nombre_canal3'] = $row['nombre_canal'];
+            array_push($return_arr,$row_array);
+           
+       }  
+   }
+   else{
+      $row_array['nombre_canal3'] = "";
+      array_push($return_arr,$row_array);
+   }
+
+   $consulta4 = "SELECT nombre_canal FROM canales where codigo='$canal4_seleccionado'";
+	$resultado4 = mysqli_query( $conexion, $consulta4 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conexion and !$canal4_seleccionado=="0")
+   {
+       while($row = mysqli_fetch_array($resultado4)){
+
+        
+	        $row_array['nombre_canal4'] = $row['nombre_canal'];
+	        array_push($return_arr,$row_array);
+          
+      }  
+   }
+   else{
+      $row_array['nombre_canal4'] = "";
+      array_push($return_arr,$row_array);
+   }
+
+   $consulta5 = "SELECT nombre_canal FROM canales where codigo='$canal5_seleccionado'";
+	$resultado5 = mysqli_query( $conexion, $consulta5 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conexion and !$canal5_seleccionado=="0")
+   {
+       while($row = mysqli_fetch_array($resultado5)){
+
+       
+            $row_array['nombre_canal5'] = $row['nombre_canal'];
+            array_push($return_arr,$row_array);
+           
+       }  
+   }
+   else{
+      $row_array['nombre_canal5'] = "";
+      array_push($return_arr,$row_array);
+   }
+
+   $consulta6 = "SELECT nombre_canal FROM canales where codigo='$canal6_seleccionado'";
+	$resultado6 = mysqli_query( $conexion, $consulta6 ) or die ( "Algo ha ido mal en la consulta a la base de datos");
+
+   if ($conexion and !$canal6_seleccionado=="0")
+   {
+       while($row = mysqli_fetch_array($resultado6)){
+
+
+            $row_array['nombre_canal6'] = $row['nombre_canal'];
+            array_push($return_arr,$row_array);
+           
+       }  
+   }
+   else{
+      $row_array['nombre_canal6'] = "";
+      array_push($return_arr,$row_array);
+   }
+mysqli_close($conexion);
+
+echo json_encode($return_arr);
+
+?>
+
+
